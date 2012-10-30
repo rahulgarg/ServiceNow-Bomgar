691e931fdb7892daa2399988288f41c0f9002c53
Huw Nolan <huw.nolan@teamultra.net>
2012-10-30 15:19:52 +0000

    Restructure BomgarConnection.js, improve error handling
    Rationalise class methods and improve error handling

--- Script Includes/BomgarConnection.js
+++ Script Includes/BomgarConnection.js
@@ -11,11 +11,14 @@ BomgarConnection.prototype = {
    initialize: function(gr_appliance) {
       
       this.log = new GSLog('tu.bomgar.loglevel','Bomgar');
-      /*
-      this.hostname = gs.getProperty( 'tu.bomgar.hostname', 'Undefined hostname property' );
-      this.username = gs.getProperty( 'tu.bomgar.username', 'Undefined username property' );
-      this.password = gs.getProperty( 'tu.bomgar.password', 'Undefined password property' );
-       */
+
+      if ( !gr_appliance || !gr_appliance.u_hostname ) {
+         throw {
+            'name': 'Invalid Bomgar Appliance',
+            'message': 'Supplied appliance: [' + gr_appliance.toString() + ']'
+         };
+      }
+
       this.hostname = gr_appliance.u_hostname;
       this.username = gr_appliance.u_api_username;
       this.password = gr_appliance.u_api_password;
@@ -26,16 +29,23 @@ BomgarConnection.prototype = {
       this.session_url = base_url + "start_session.ns";
       this.report_url  = base_url + "reporting.ns";
       this.backup_url  = base_url + "backup.ns";
+
+      this.connectTimeout = gs.getProperty( 'tu.bomgar.connect.timeout', 10 ) * 1000;
+      this.socketTimeout = gs.getProperty( 'tu.bomgar.socket.timeout', 10 ) * 1000;
       
       this.apiParams = null;
       this.httpStatus = null;
       this.responseDoc = ''; // a string of the response
+      this.responseObj = null; // a JavaScript object from the XML response
       this.postMethod = null;
       this.errorMessage = '';
-      this.requestHeaders = {};
       
    },
    
+   //---------------------------------------------
+   // Public method definitions
+   //---------------------------------------------
+
    sendCommand: function( params ) {
       this.request_url = this.command_url;
       this.apiParams = params;
@@ -53,7 +63,37 @@ BomgarConnection.prototype = {
       this.apiParams = params;
       return this._postRequest();
    },
+
+   //---------------------------------------------
    
+   getRequestURL: function() {
+      return this.request_url;
+   },
+   
+   getHttpStatus: function() {
+      return this.httpStatus;
+   },
+   
+   getResponseDoc: function() {
+      return this.responseDoc;
+   },
+   
+   getResponseObject: function() {
+      return this.responseObj;
+   },
+   
+   getResponseRootName: function() {
+      return this.responseRoot;
+   },
+   
+   getErrorMessage: function() {
+      return this.errorMessage;
+   },
+   
+   //---------------------------------------------
+   // Internal method definitions
+   //---------------------------------------------
+
    /**
     * Post the request to the Bomgar appliance
     * Returns a JS object representation of the XML response
@@ -61,26 +101,31 @@ BomgarConnection.prototype = {
    _postRequest: function() {
       
       var i, msg = "Post Request";
+      var msg_sep = '\n----------------------------------------\n';
+
+      msg += "\nRequest URL: [" + this.request_url + "]";
+      msg += "\nTimeouts: [" + this.connectTimeout + "],[" + this.socketTimeout + "]";
       
       this.httpStatus = null;
       this.errorMessage = null;
       this.responseDoc = null;
+      this.responseObj = null;
+      this.responseRoot = null;
       this.postParams = '';
       this.postResult = '';
       
-      // must initialize first, to get extended protocols and SSL context
-      // before making a new PostMethod
-      var httpClient = new Packages.com.glide.communications.HTTPClient();
-      
-      // Set connection and socket timeouts to 10 secs
-      var httpParams = httpClient.getParams();
-      httpParams.setConnectionTimeout(10000);
-      httpParams.setSoTimeout(10000);
-      
       try {
+
+         // must initialize first, to get extended protocols and SSL context
+         // before making a new PostMethod
+         var httpClient = new Packages.com.glide.communications.HTTPClient();
+         
+         // Set connection and socket timeouts
+         var httpParams = httpClient.getParams();
+         httpParams.setConnectionTimeout( this.connectTimeout );
+         httpParams.setSoTimeout( this.socketTimeout );
          
          var pm = new Packages.org.apache.commons.httpclient.methods.PostMethod( this.request_url );
-         msg += "\nRequest URL: ["+this.request_url+"]";
          this.postMethod = pm;
          
          // Add credentials to all POSTs
@@ -89,62 +134,30 @@ BomgarConnection.prototype = {
          pm.addParameter("username", this.username);
          pm.addParameter("password", clearpwd);
          
-         //		 this.postParams += "\nParam: [username] = ["+this.username+"]";
-         //		 this.postParams += "\nParam: [password] = ["+clearpwd+"]";
+         //     this.postParams += "\nParam: [username] = ["+this.username+"]";
+         //     this.postParams += "\nParam: [password] = ["+clearpwd+"]";
          
          // Add action specific params
          for ( i=0; i < this.apiParams.length; i++ ) {
             pm.addParameter( this.apiParams[i][0], this.apiParams[i][1] );
             this.postParams += "\nParam: ["+this.apiParams[i][0]+"] = ["+this.apiParams[i][1]+"]";
          }
-         
-         msg += this.postParams;
-         msg += '\n----------------------------------------';
-         
+
+         msg += this.postParams + msg_sep;
+
+         //-------------------------------------------------
+         // Make the HTTP Request to the Bomgar Appliance
+         //-------------------------------------------------
          var result = httpClient.executeMethod( pm );
-         
-         /*
-         msg = tu.debug.ExpressionDump( result, 'HTTP Result', true );
-         this.log.logInfo( msg );
-         
-         var rh = pm.getResponseHeaders();
-         msg = "HTTP Response headers (" + rh.length + ")";
-         msg += "\n" + pm.getStatusLine();
-         for ( i=0; i<rh.length; i++ ) {
-            msg += "\n" + rh[i].getName() + " : " + rh[i].getValue();
-         }
-         this.log.logInfo( msg );
-          */
          
          this.httpStatus = result;
          this.errorMessage = httpClient.getErrorMessage();
-         this.responseDoc = "" + pm.getResponseBodyAsString();
+         this.responseDoc = '' + pm.getResponseBodyAsString();
          
-         this.postResult = "\nHTTP Status:   ["+this.httpStatus+"]";
-         this.postResult += "\nError Message: ["+this.errorMessage+"]";
-         this.postResult += "\nResponse Doc Length:  ["+this.responseDoc.length+"]";
-         
-         msg += "\nPost Response" + this.postResult;
-         if ( this.log.debugOn() ) {
-            msg += "\nResponse Doc:  ["+this.responseDoc+"]";
-         }
-         this.log.logInfo(msg);
-         
-         pm.releaseConnection();
-         
-         if ( this.httpStatus != 200 ) {
-            return null;
-         }
-         
-      } catch (e) {
-         
-         this.errorMessage = e.toString();
-         
-         msg = "Failed to communicate with Bomgar appliance";
-         msg += "\nError: " + this.errorMessage;
-         msg += "\nRequest URL: " + this.request_url;
-         msg += this.postParams;
-         
+      } catch (http_exception) {
+
+         this.errorMessage = '' + http_exception.toString();
+         msg = "ERROR: " + this.errorMessage + msg_sep + msg;
          if ( this.log.debugOn() ) {
             var sb = new Packages.java.lang.StringBuffer();
             var st = e.getStackTrace();
@@ -153,21 +166,47 @@ BomgarConnection.prototype = {
             }
             msg += "\n" + sb.toString();
          }
-         
          this.log.logError(msg);
-         
          return null;
-         
+
       } finally {
          this.postMethod.releaseConnection();
       }
+
+
+      this.postResult = "\nHTTP Status:   ["+this.httpStatus+"]";
+      this.postResult += "\nError Message: ["+this.errorMessage+"]";
+      this.postResult += "\nResponse Doc Length:  ["+this.responseDoc.length+"]";
+      
+      msg += "\nPost Response" + this.postResult;
+      if ( this.log.debugOn() ) {
+         msg += "\nResponse Doc:  ["+this.responseDoc+"]";
+      }
+      
+      if ( this.httpStatus != 200 ) {
+         this.errorMessage = "Received unexpected HTTP Status (" + 
+                              this.httpStatus + ") from Bomgar appliance";
+         msg = "ERROR: " + this.errorMessage + "\n" + msg + msg_sep;
+         this.log.logError(msg);
+         return null;
+      }
+      
+      this.log.logInfo(msg);
       
       // Get the name of the XMl root element
       // (this is lost during the standard XMLHelper toObject call)
       this.responseRoot = this._getRootName();
       
       // Convert the response to a JS object
-      this.responseObj = new XMLHelper(this.responseDoc).toObject();
+      try {
+         this.responseObj = new XMLHelper(this.responseDoc).toObject();
+      } catch (xml_exception) {
+         this.errorMessage = "Failed to convert Bomgar XML to JavaScript object";
+         this.errorMessage += '\n' + xml_exception.toString();
+         msg = "ERROR: " + this.errorMessage + msg_sep + msg;
+         this.log.logError(msg);
+         return null;
+      }
       
       return this.responseObj;
       
@@ -185,57 +224,10 @@ BomgarConnection.prototype = {
          if ( res.length >= 2 ) {
             return res[1];
          }
-      } catch(e) {
+      } catch(regex_exception) {
          return null;
       }
       
-   },
-   
-   /**
-    * get the response string
-    */
-   getResponseDoc: function() {
-      return this.responseDoc;
-   },
-   
-   getRequestURL: function() {
-      return this.request_url;
-   },
-   
-   getResponseHeaderValue: function(headerName) {
-      var header = this.postMethod.getResponseHeader(headerName);
-      return header.getValue();
-   },
-   
-   setRequestHeader: function(headerName, headerValue) {
-      this.requestHeaders[headerName]=headerValue;
-   },
-   
-   /**
-    * get the HTTP status of the post
-    */
-   getHttpStatus: function() {
-      return this.httpStatus;
-   },
-   
-   /**
-    * get the error message as a result of the post
-    */
-   getErrorMessage: function() {
-      return this.errorMessage;
-   },
-   
-   /**
-    * encode basic auth credentials, to be used when invoking SOAP
-    * through the ECC queue
-    */
-   encodeCredentials: function() {
-      if (this.userName == null)
-         return "";
-      
-      var str = this.userName + ":" + this.password;
-      var e = new Packages.com.glide.util.Encrypter();
-      return e.encrypt(str);
    },
    
    type : 'BomgarConnection'
