0383b087d9df83e87c05d8862ae24730de76cee3
Huw Nolan <huw.nolan@teamultra.net>
2013-06-06 09:52:17 +0100

    Fix - ensure Session Refresh picks up correct apliance.

--- UI Actions/u_tu_bg_session/Refresh Session.js
+++ UI Actions/u_tu_bg_session/Refresh Session.js
@@ -1,7 +1,7 @@
 refresh_session_data();
 
 function refresh_session_data() {
-   bg = new BomgarAPI();
+   bg = new BomgarAPI( current.u_appliance.u_hostname );
    ss = bg.retrieveSession( current.u_lsid );
    if (!ss) {
       gs.addErrorMessage( bg.getErrorMessage() );
