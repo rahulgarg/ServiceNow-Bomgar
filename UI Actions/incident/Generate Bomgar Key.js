e8e871900bbab1184f615333430b0b6c09ddea99
Huw Nolan <huw.nolan@teamultra.net>
2013-06-13 18:11:05 +0100

    Added property and event to control Session Key email
    - Added bomgar.session.keygen event
    - Added tu.bomgar.session.keygen property.
    - Amended UI action to use these features

--- UI Actions/incident/Generate Bomgar Key.js
+++ UI Actions/incident/Generate Bomgar Key.js
@@ -5,8 +5,8 @@ function generate_session_key() {
    var sk = bg.generateSessionKey(current.number);
 
    if ( !sk ) {
-	  gs.addErrorMessage( 'Failed to generate Bomgar Session Key' );
-	  return;
+     gs.addErrorMessage( 'Failed to generate Bomgar Session Key' );
+     return;
    }
    
    var exp = bg.getGlideDateTime(sk.expires);
@@ -19,15 +19,22 @@ function generate_session_key() {
    msg += "\nAlternative URL:   " + sk.key_url;
 
    current.work_notes = msg;
-   
-   msg = "The support engineer, " + current.assigned_to.getDisplayValue();
-   msg += ", would like to establish a remote support session to your device "
-   msg += "in order to progress incident number, " + current.number + ". ";
-   msg += "\nThe assigned session key is : " + sk.short_key;
-   msg += "\nTo assist them in establishing this support session, please follow the link below.\n\n"
-   
-   msg += sk.mail_body;
-   current.comments = msg;
+
+   var keygen_email = gs.getProperty( 'tu.bomgar.keygen.email', 'none' );
+
+   if ( keygen_email == 'custom' ) {
+      // Send the event to trigger a custom email
+      gs.eventQueue("bomgar.session.keygen", current, sk.short_key, sk.key_url);
+   } else if ( keygen_email == 'comment' ) {
+      // Add a customer visible comment (which may trigger an email)
+      msg = "The support engineer, " + current.assigned_to.getDisplayValue();
+      msg += ", would like to establish a remote support session to your device ";
+      msg += "in order to progress incident number, " + current.number + ". ";
+      msg += "\nThe assigned session key is : " + sk.short_key;
+      msg += "\nTo assist them in establishing this support session, please follow the link below.\n\n";
+      msg += sk.mail_body;
+      current.comments = msg;
+   }
 
    action.setRedirectURL(current);
    current.update();
